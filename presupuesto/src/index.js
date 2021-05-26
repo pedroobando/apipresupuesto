5205279372844a60d0e3fd4e3c5d928775c0bd33
pedroobando <pedroobando@hotmail.com>
2021-05-26 16:45:54 -0400

    prueba de cuentas

--- presupuesto/src/index.js
+++ presupuesto/src/index.js
@@ -1,5 +1,6 @@
 const cuenta = require("../json/cuentas.json");
 
-ctaAno = cuenta.filter((cta) => cta.Año == 2019);
+ctaAno = cuenta.filter((cta) => cta.Año == 2019 && cta.Nivel == 4 && cta.Tipo == "D");
 
 console.log(ctaAno);
+console.log(ctaAno.length);
--- readmdb/src/conectDb.js
+++ readmdb/src/conectDb.js
@@ -1,4 +1,5 @@
 'use strict';
+'use strict';
 
 const ADODB = require('node-adodb');
 
