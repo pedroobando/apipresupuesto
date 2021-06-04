3322f51f3e029183f0abb5027468180f0398f3b6
Pedro Obando <pedroobando@hotmail.com>
2021-06-03 20:51:24 -0400

    configuracion env

--- apirpresup/src/controllers/readmdb.controller.js
+++ apirpresup/src/controllers/readmdb.controller.js
@@ -7,23 +7,16 @@ const { cuentaPresupuesto } = require('../calculos/presupuesto.calculo');
 
 const { query } = require('../database/dbcnn');
 
-const cnndb = './PresupuestoData2013.accdb';
+const thePort = process.env.PORT || 8080;
+const cnndb = process.env.ACCDB || './PresupuestoData2013.accdb';
 const orderBy = 'ORDER BY Año, Part, Gene, Espe, Sub';
-
-const messageError = () =>
-  res.status(500).json({
-    ok: false,
-    data: {
-      message: 'Consulte con el administrador',
-    },
-  });
 
 const getCuenta = async (req, res = response) => {
   const { year } = req.params;
   try {
     const resultJson = await query(
       cnndb,
-      `Select * from Cuentas where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Cuentas where Año = ${year} ${orderBy}`
     );
     const resultCuenta = cuentaPresupuesto(resultJson);
     return res.status(200).json(resultCuenta);
@@ -43,11 +36,11 @@ const getModificado = async (req, res = response) => {
   try {
     const resultPresup = await query(
       cnndb,
-      `Select * from Cuentas where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Cuentas where Año = ${year} ${orderBy}`
     );
     const resultModif = await query(
       cnndb,
-      `Select * from Modificaciones where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Modificaciones where Año = ${year} ${orderBy}`
     );
     const resultCuenta = cuentaModificacion(resultPresup, resultModif);
     return res.status(200).json(resultCuenta);
@@ -69,11 +62,11 @@ const getCompromiso = async (req, res = response) => {
   try {
     const resultPresup = await query(
       cnndb,
-      `Select * from Cuentas where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Cuentas where Año = ${year} ${orderBy}`
     );
     const resultCompromiso = await query(
       cnndb,
-      `Select * from Comprometido where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Comprometido where Año = ${year} ${orderBy}`
     );
     const resultCuenta = cuentaCompromiso(resultPresup, resultCompromiso);
     return res.status(200).json(resultCuenta);
@@ -95,11 +88,11 @@ const getCausado = async (req, res = response) => {
   try {
     const resultPresup = await query(
       cnndb,
-      `Select * from Cuentas where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Cuentas where Año = ${year} ${orderBy}`
     );
     const resultCausado = await query(
       cnndb,
-      `Select * from Causado where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Causado where Año = ${year} ${orderBy}`
     );
     const resultCuenta = cuentaCausado(resultPresup, resultCausado);
     return res.status(200).json(resultCuenta);
@@ -121,11 +114,11 @@ const getPagado = async (req, res = response) => {
   try {
     const resultPresup = await query(
       cnndb,
-      `Select * from Cuentas where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Cuentas where Año = ${year} ${orderBy}`
     );
     const resultPagado = await query(
       cnndb,
-      `Select * from Pagos where Año >= ${year} and Año <= ${year} ${orderBy}`
+      `Select * from Pagos where Año = ${year} ${orderBy}`
     );
     const resultCuenta = cuentaPagado(resultPresup, resultPagado);
     return res.status(200).json(resultCuenta);
